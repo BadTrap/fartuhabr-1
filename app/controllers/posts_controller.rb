8d1029cbdf08e903920a3d3a6888642e6361d58a
Peter Markovich <bigzontv@gmail.com>
2017-06-09 17:40:33 +0300

    Настройка девайса(1 частина)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @posts = Post.paginate(page: params[:page], per_page: 5)
   end
