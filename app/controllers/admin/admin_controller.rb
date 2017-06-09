15d65dc53224e5596dc9fa6d7cafb718b769a830
Peter Markovich <bigzontv@gmail.com>
2017-06-09 19:56:19 +0300

    Завершення роботи з девайсом

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,6 +2,8 @@ class Admin::AdminController < ApplicationController
 before_action :authenticate_user!
 before_action :check_admin
 
+#layout "admin"
+
 protected
 def check_admin
   redirect_to root_path, alert: "У Вас нема прав для доступа до даної сторінки сайту" unless current_user.admin?
